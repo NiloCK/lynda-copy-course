88af1ea894adfaf10271ee17b52d9e8753b582f2
Colin Kennedy <colinkennedy@hotmail.com>
2017-04-03 21:39:45 -0600

    Pruning

--- src/index.ts
+++ src/index.ts
@@ -12,22 +12,6 @@ class HelloW {
             console.log(row);
         })
 
-        //        sourceDB.close();
-
-        // console.log(process.version);
-
-
-        // console.log("Hello World!");
-        // console.log("Hello World!");
-        // console.log("Hello World!");
-        // let reader = r.createInterface({
-        //     input: process.stdin,
-        //     output: process.stdout
-        // })
-        // let name = reader.question("What's your name?", function (resp: string) {
-        //     console.log("Hi " + resp)
-        // });
-
         return 0;
     }
 }
