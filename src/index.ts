364a875e690eb7c95fccbceddaf6059828704187
Colin Kennedy <colinkennedy@hotmail.com>
2017-04-03 21:38:24 -0600

    'Hello sourceDB' - opening and querying the db

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,32 @@
 import * as r from 'readline'
-
+import * as sqlite3 from 'sqlite3'
 
 class HelloW {
     public static main(): number {
-        console.log(process.version);
-
-
-        console.log("Hello World!");
-        console.log("Hello World!");
-        console.log("Hello World!");
-        let reader = r.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        })
-        let name = reader.question("What's your name?", function (resp: string) {
-            console.log("Hi " + resp)
+        let sourceDB = new sqlite3.Database('db.sqlite', sqlite3.OPEN_READONLY, (err: Error) => {
+            console.log(err);
         });
+
+        sourceDB.each("select * from Author", function (err, row) {
+
+            console.log(row);
+        })
+
+        //        sourceDB.close();
+
+        // console.log(process.version);
+
+
+        // console.log("Hello World!");
+        // console.log("Hello World!");
+        // console.log("Hello World!");
+        // let reader = r.createInterface({
+        //     input: process.stdin,
+        //     output: process.stdout
+        // })
+        // let name = reader.question("What's your name?", function (resp: string) {
+        //     console.log("Hi " + resp)
+        // });
 
         return 0;
     }
